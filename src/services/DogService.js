7adcf3182464633990d63f81546908e1e613e6b3
wenjun.911119 <wenjun.911119@bytedance.com>
2020-09-01 01:45:02 +0800

    feat: 取消报错

--- src/services/DogService.js
+++ src/services/DogService.js
@@ -3,13 +3,13 @@ const { Service } = require('@ruiyun/fastapi')
 class DogService extends Service {
   async add({ name, age, color }) {
     const id = Math.round(Math.random() * 10000000)
-    if (id % 3 === 2) {
-      // 随机报错
-      throw new Error('生成dogId失败')
-    } else if (id % 3 === 1) {
-      // eslint-disable-next-line
-      console.log(a.b)
-    }
+    // if (id % 3 === 2) {
+    //   // 随机报错
+    //   throw new Error('生成dogId失败')
+    // } else if (id % 3 === 1) {
+    //   // eslint-disable-next-line
+    //   console.log(a.b)
+    // }
     return {
       name,
       age,
